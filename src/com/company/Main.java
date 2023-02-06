91b982598d2cf8b6c02c44af151894557396fdaf
micserv010821 <dotnet290320@gmail.com>
2023-02-06 18:53:58 +0200

    upload targil

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,6 +26,18 @@ public class Main {
 
         Car toyota = new Car(); // create new car instance
 
+        // targilon:
+        // 1. create an instance of your car with your car details
+        //    if you do not have a car then make up a dream car
+        // 2. add a function called fuelUp in class Car
+        //    which prints "filling up fuel."
+        //    execute this function from main
+        // 3. create a class called Dog
+        //    place data: name, color
+        // 4. create an instance of Dog called Rexi with a brown color
+        // 5. create an instance of Dog called Max with a white color
+        // 6. create a method called bark which print "haw haw..."
+        // 7. execute the bark function from main
 
 
     }
